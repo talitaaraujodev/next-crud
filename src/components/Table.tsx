b720081f6d89cd21b431519d049225f3b871f383
TalitaCumi <talitacumi.araujo@gmail.com>
2022-10-31 10:03:41 -0300

    added icons e estilizado component Table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,32 +1,86 @@
 import Cliente from "../core/Cliente";
+import { IconDelete } from "./IconDelete";
+import { IconEdit } from "./IconEdit";
+import { IconEye } from "./IconEye";
 
 interface TableProps {
   clientes: Cliente[];
+  clienteSelected?: (cliente: Cliente) => void;
+  clienteExcluded?: (clinte: Cliente) => void;
+  clienteVizualize?: (cliente: Cliente) => void;
 }
 export default function Table(props: TableProps) {
+  const showActions =
+    props.clienteExcluded || props.clienteSelected || props.clienteExcluded;
+
   const renderHeader = () => {
     return (
       <tr>
-        <th>Código</th>
-        <th>Nome</th>
-        <th>Idade</th>
+        <th className="text-left p-4">Código</th>
+        <th className="text-left p-4">Nome</th>
+        <th className="text-left p-4">Idade</th>
+        {showActions ? <th className=" text-left p-4">Ações</th> : false}
       </tr>
     );
   };
   const renderData = () => {
     return props.clientes?.map((cliente, index) => {
       return (
-        <tr key={cliente.id}>
-          <td>{cliente.id}</td>
-          <td>{cliente.nome}</td>
-          <td>{cliente.idade}</td>
+        <tr
+          key={cliente.id}
+          className={`${index % 2 === 0 ? "bg-purple-200" : "bg-purple-100"}`}
+        >
+          <td className="text-left p-4">{cliente.id}</td>
+          <td className="text-left p-4">{cliente.nome}</td>
+          <td className="text-left p-4">{cliente.idade}</td>
+          {showActions ? renderActions(cliente) : false}
         </tr>
       );
     });
   };
+  const renderActions = (cliente: Cliente) => {
+    return (
+      <td className="flex justify-center">
+        {props.clienteSelected ? (
+          <button
+            onClick={() => props.clienteSelected?.(cliente)}
+            className={`flex justify-center items-center text-green-600 rounded-full hover:bg-purple-50 p-2 m-1`}
+          >
+            {IconEdit}
+          </button>
+        ) : (
+          false
+        )}
+        {props.clienteExcluded ? (
+          <button
+            onClick={() => props.clienteExcluded?.(cliente)}
+            className={`flex justify-center items-center text-red-600 rounded-full hover:bg-purple-50 p-2 m-1`}
+          >
+            {IconDelete}
+          </button>
+        ) : (
+          false
+        )}
+        {props.clienteVizualize ? (
+          <button
+            onClick={() => props.clienteVizualize?.(cliente)}
+            className={`flex justify-center items-center text-blue-600 rounded-full hover:bg-purple-50 p-2 m-1`}
+          >
+            {IconEye}
+          </button>
+        ) : (
+          false
+        )}
+      </td>
+    );
+  };
   return (
-    <table>
-      <thead>{renderHeader()}</thead>
+    <table className="w-full rounded-xl overflow-hidden">
+      <thead
+        className={`bg-gradient-to-r from-purple-500 to-purple-800 text-gray-100`}
+      >
+        {renderHeader()}
+      </thead>
 
       <tbody>{renderData()}</tbody>
     </table>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,31 @@ import Table from "../components/Table";
 import Cliente from "../core/Cliente";
 
 export default function Home() {
-  const clientes = [new Cliente("1adf", "Ana", 20)];
+  const clientes = [
+    new Cliente("1adf", "Ana", 20),
+    new Cliente("10d8", "Creuza", 25),
+    new Cliente("25df", "Jubileu", 42),
+  ];
+  function clienteSelected(cliente: Cliente) {
+    console.log("Editando...", cliente);
+  }
+  function clienteExcluded(cliente: Cliente) {
+    console.log("Excluindo...", cliente);
+  }
+  function clienteVizualize(cliente: Cliente) {
+    console.log("Visualizando...", cliente);
+  }
   return (
     <div
       className={`flex justify-center items-center h-screen bg-gradient-to-r from-blue-500 to-purple-500`}
     >
       <Layout title="Clientes">
-        <Table clientes={clientes} />
+        <Table
+          clientes={clientes}
+          clienteSelected={clienteSelected}
+          clienteExcluded={clienteExcluded}
+          clienteVizualize={clienteVizualize}
+        />
       </Layout>
     </div>
   );
